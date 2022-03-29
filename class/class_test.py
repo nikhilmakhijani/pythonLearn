461bedcf155a7e048ef55a9fb98bf0e41d82ed92
Nikhil Makhijani <Nikhilmakhijani2893@gmail.com>
2022-03-29 19:42:29 +0530

    removing stale snippet from class_test.py

--- class/class_test.py
+++ class/class_test.py
@@ -8,10 +8,3 @@ newEmp = Employee()
 print(newEmp.company)
 Employee.company ="yours" 
 print(newEmp.company) # will print "yours"
-
-n = int(input("Enter the number of employees"))
-for i in range(n):
-    newEmp[i] = Employee()
-
-for emp in newEmp:
-    print(emp)
