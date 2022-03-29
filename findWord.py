bb990a908f9e1b260caf30efcc9dae78dadce91d
Nikhil Makhijani <Nikhilmakhijani2893@gmail.com>
2022-03-29 14:13:38 +0530

    adding script to search a word

--- findWord.py
+++ findWord.py
@@ -11,17 +11,21 @@ def check_path(file):
         print("file {} doesnt exist".format(file))
         sys.exit(1)
 
-def print_content(file):
+def print_content(file,word):
     check_path(file)
     with open(file, 'r') as fl:
-      print(fl.read())
+     if word in fl.read():
+         print ("Yes, word - {} exists".format(word))
+     else:
+         print( "word - {} not found".format(word))
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input',help="input line",type=str,required=True)
     args = parser.parse_args()
     input_data= args.input
-    print_content(input_data) 
+    word = input("Enter the word you want to search: ")
+    print_content(input_data,word) 
 
 if __name__=='__main__':
     main()
