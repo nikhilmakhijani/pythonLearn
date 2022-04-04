9f6ba1f24dda3061b0d1fadbeefc602061b6356a
Nikhil Makhijani <Nikhilmakhijani2893@gmail.com>
2022-04-04 18:08:29 +0530

    multiple exceptions

---
+++ errorHandling/differentExceptions.py
@@ -0,0 +1,17 @@
+from decimal import DivisionByZero
+ 
+while(True):
+ num1 = input("Enter your number or q to exit: ")
+ if num1 == 'q' or num1 == 'Q':
+     break
+ else:
+  try:
+   result = 100/int(num1)
+   print("your result is: "+str(result))
+  except ValueError as e:
+   print("Please provide an integer")
+  except DivisionByZero as e:
+   print("You can not divide by zero")
+
+print("thanks for trying")
+
